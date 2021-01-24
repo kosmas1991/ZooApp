f9db8722ae95a54782cc27b5e6ec442df8bc9769
kosmas1991 <kosmasgou@gmail.com>
2021-01-24 11:51:47 +0200

    added 2nd activity screen

--- app/src/main/java/gr/techzombie/zooapp/Animal.kt
+++ app/src/main/java/gr/techzombie/zooapp/Animal.kt
@@ -4,9 +4,11 @@ class Animal {
     var name:String?=null
     var des:String?=null
     var image:Int?=null
-    constructor(name:String,des:String,image:Int){
+    var isKiller:Boolean?=null
+    constructor(name:String,des:String,image:Int,isKiller:Boolean){
         this.name = name
         this.des = des
         this.image = image
+        this.isKiller = isKiller
     }
 }
--- app/src/main/java/gr/techzombie/zooapp/MainActivity.kt
+++ app/src/main/java/gr/techzombie/zooapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package gr.techzombie.zooapp
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
@@ -16,34 +17,75 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var adapter:AnimalsAdapter?=null
+        var adapter: AnimalsAdapter? = null
         var listOfAnimals = ArrayList<Animal>()
-        listOfAnimals.add(Animal("Baboon","Baboon lives at the zoo with other animals",R.drawable.baboon))
-        listOfAnimals.add(Animal("Bulldog","Bulldog lives at the zoo with other animals",R.drawable.bulldog))
-        listOfAnimals.add(Animal("Panda","Panda lives at the zoo with other animals",R.drawable.panda))
-        listOfAnimals.add(Animal("Swallow bird","Swallow bird lives at the zoo with other animals",R.drawable.swallow_bird))
-        listOfAnimals.add(Animal("White tiger","White tiger lives at the zoo with other animals",R.drawable.white_tiger))
-        listOfAnimals.add(Animal("Zebra","Zebra lives at the zoo with other animals",R.drawable.zebra))
-        adapter= AnimalsAdapter(this,listOfAnimals)
+        listOfAnimals.add(Animal("Baboon", "Baboon lives at the zoo with other animals", R.drawable.baboon, false))
+        listOfAnimals.add(Animal("Bulldog", "Bulldog lives at the zoo with other animals", R.drawable.bulldog, true))
+        listOfAnimals.add(Animal("Panda", "Panda lives at the zoo with other animals", R.drawable.panda, true))
+        listOfAnimals.add(
+            Animal(
+                "Swallow bird",
+                "Swallow bird lives at the zoo with other animals",
+                R.drawable.swallow_bird,
+                false
+            )
+        )
+        listOfAnimals.add(
+            Animal(
+                "White tiger",
+                "White tiger lives at the zoo with other animals",
+                R.drawable.white_tiger,
+                true
+            )
+        )
+        listOfAnimals.add(Animal("Zebra", "Zebra lives at the zoo with other animals", R.drawable.zebra, false))
+        adapter = AnimalsAdapter(this, listOfAnimals)
         tvListAnimal.adapter = adapter
     }
 
-    class AnimalsAdapter:BaseAdapter{
-        var context:Context?=null
-        var listOfAnimals=ArrayList<Animal>()
-        constructor(context:Context, listOfAnimals:ArrayList<Animal>):super(){
+    class AnimalsAdapter : BaseAdapter {
+        var context: Context? = null
+        var listOfAnimals = ArrayList<Animal>()
+
+        constructor(context: Context, listOfAnimals: ArrayList<Animal>) : super() {
             this.listOfAnimals = listOfAnimals
-            this.context=context
+            this.context = context
         }
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
             var animal = listOfAnimals[position]
-            var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            var myView = inflator.inflate(R.layout.animal_ticket,null)
-            myView.image.setImageResource(animal.image!!)
-            myView.title.text = animal.name
-            myView.description.text = animal.des
-            return myView
+            if (animal.isKiller!!) {
+                var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                var myView = inflator.inflate(R.layout.animal_ticket_killer, null)
+                myView.image.setImageResource(animal.image!!)
+                myView.title.text = animal.name
+                myView.description.text = animal.des
+                myView.setOnClickListener {
+                    val intent = Intent(context,AnimalInfo::class.java)
+                    intent.putExtra("title",animal.name!!)
+                    intent.putExtra("des",animal.des!!)
+                    intent.putExtra("image",animal.image!!)
+                    context!!.startActivity(intent)
+
+                }
+                return myView
+            } else {
+                var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                var myView = inflator.inflate(R.layout.animal_ticket, null)
+                myView.image.setImageResource(animal.image!!)
+                myView.title.text = animal.name
+                myView.description.text = animal.des
+                myView.setOnClickListener {
+                    val intent = Intent(context, AnimalInfo::class.java)
+                    intent.putExtra("title", animal.name!!)
+                    intent.putExtra("des", animal.des!!)
+                    intent.putExtra("image", animal.image!!)
+                    context!!.startActivity(intent)
+                }
+                return myView
+            }
+
+
         }
 
         override fun getItem(position: Int): Any {
